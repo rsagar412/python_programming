a8179b7c66bc5a6958c7bebdb3ab56f5f3f9444d
Sagar Rathod <rathod.sagar23@yahoo.com>
2025-01-04 22:31:07 +0530

    testmessage

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Welcome to the Python Programming")
+
+# Tuples   :  Immutable, the items in the tuple cannot be changed
+tupdemo = (1,2,3)
+print(tupdemo)
+print(type(tupdemo))
+print(len(tupdemo),'\n', tupdemo[1])
